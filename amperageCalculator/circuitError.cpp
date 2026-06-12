17855c121ea8f1c6f9d12a755bf1ae65981b8558
Anna Bezhenar <catty5647starry@gmail.com>
2026-06-13 00:02:17 +0300

    Улучшен формат вывода сообщений об ошибках

--- amperageCalculator/circuitError.cpp
+++ amperageCalculator/circuitError.cpp
@@ -18,7 +18,6 @@ void Error::setError(ErrorType t, const std::string& compName, int ln, const std
     type = t;
     componentName = compName;
     line = ln;
-    invalidValue = detail;
 }
 
 bool Error::hasError() const {
@@ -27,7 +26,12 @@ bool Error::hasError() const {
 }
 
 std::string Error::getMessage() const {
-    // Формирование пользовательского текстового описания ошибки
+    std::string prefix;
+
+    if (line > 0) {
+        prefix = "Строка " + std::to_string(line) + ": ";
+    }
+
     switch (type) {
     case ErrorType::InputFileNotExist:
         return "Неверно указан файл с входными данными. Возможно, файл не существует.";
@@ -36,47 +40,45 @@ std::string Error::getMessage() const {
     case ErrorType::FileEmpty:
         return "Неверно указан файл с входными данными. Файл пуст.";
     case ErrorType::MissingDigraph:
-        return "Неверный формат входного файла. Ожидается 'digraph' в начале файла.";
+        return "Ожидается 'digraph' в начале файла.";
     case ErrorType::MissingOpenBrace:
-        return "Неверный формат входного файла. Отсутствует открывающая фигурная скобка '{'.";
+        return "Отсутствует открывающая фигурная скобка '{'.";
     case ErrorType::MissingCloseBrace:
-        return "Неверный формат входного файла. Отсутствует закрывающая фигурная скобка '}'.";
+        return "Отсутствует закрывающая фигурная скобка '}'.";
     case ErrorType::InvalidConnectionSyntax:
-        return "Не соответствует формату записи связи.";
+        return prefix + "не соответствует формату записи связи.";
     case ErrorType::InvalidComponentName:
-        return "Некорректное имя компонента \"" + componentName + "\". Имя должно содержать только буквы и цифры и быть не более 7 символов длиной.";
+        return prefix + "некорректное имя компонента \"" + componentName + "\". Имя должно содержать только буквы и цифры и быть не более 7 символов.";
     case ErrorType::DuplicateComponent:
-        return "Компонент с таким именем уже существует. Компонент \"" + componentName + "\" уже был определен ранее.";
-    case ErrorType::TooManyParallelElements:
-        return "Граф задан некорректно. В цепи должно быть не более 10 взаимно-параллельных элементов.";
+        return prefix + "компонент \"" + componentName + "\" уже был определен ранее.";
     case ErrorType::MissingLabelAttribute:
-        return "Отсутствует описание компонента. Отсутствует атрибут label у компонента \"" + componentName + "\".";
+        return prefix + "отсутствует атрибут label у компонента \"" + componentName + "\".";
     case ErrorType::EmptyLabel:
-        return "Пустое описание компонента. Пустой label у компонента \"" + componentName + "\".";
+        return prefix + "пустой label у компонента \"" + componentName + "\".";
     case ErrorType::UnknownComponentType:
-        return "Неизвестный тип компонента. Ожидается: SOURCE, R, L или C.";
+        return prefix + "неизвестный тип компонента. Ожидается SOURCE, R, L или C.";
     case ErrorType::IncompleteSource:
-        return "Неполное описание источника. Ожидается: SOURCE frequency=<значение> voltage=<значение> phase=<значение>.";
+        return prefix + "неполное описание источника. Ожидается: SOURCE frequency=<значение> voltage=<значение> phase=<значение>.";
     case ErrorType::MissingComponentValue:
-        return "Неполное описание компонента.";
+        return prefix + "неполное описание компонента \"" + componentName + "\".";
     case ErrorType::InvalidNumberFormat:
-        return "Параметр элемента должен быть числом.";
-    case ErrorType::ValueOutOfRange:
-        return "Параметр вне допустимого диапазона.";
+        return prefix + "значение не является корректным числом.";
     case ErrorType::PhaseOutOfRange:
-        return "Параметр вне допустимого диапазона. Фаза должна быть в диапазоне [0, 360].";
+        return "Фаза должна находиться в диапазоне [0, 360].";
     case ErrorType::InvalidExponentialFormat:
-        return "Параметр в экспоненциальной форме записан некорректно.";
+        return prefix + "некорректная экспоненциальная запись.";
     case ErrorType::UndefinedComponent:
-        return "Неопределенный компонент. Компонент используется в соединении, но не описан.";
+        return "Компонент \"" + componentName + "\" используется в соединении, но не был описан.";
     case ErrorType::CircuitNotClosed:
         return "Цепь не замкнута. Необходимо, чтобы существовал путь от источника через все элементы обратно к источнику, следуя по направлению стрелок.";
     case ErrorType::IsolatedComponent:
-        return "Нарушение замкнутости цепи. В цепи не может быть изолированных элементов.";
-    case ErrorType::SelfConnection:
-        return "Некорректное соединение. Соединение компонента с самим собой недопустимо.";
+        return "Обнаружен изолированный компонент \"" + componentName + "\".";
     case ErrorType::DuplicateConnection:
         return "Дублирующееся соединение.";
+    case ErrorType::SelfConnection:
+        return "Cоединение компонента \"" + componentName + "\" с самим собой недопустимо.";
+    case ErrorType::ValueOutOfRange:
+        return "Значение находится вне допустимого диапазона.";
     case ErrorType::NoConnections:
         return "Цепь не содержит ни одного соединения между компонентами.";
     case ErrorType::NoComponents:
@@ -85,6 +87,8 @@ std::string Error::getMessage() const {
         return "Отсутствует источник тока. Цепь должна содержать ровно один элемент типа SOURCE.";
     case ErrorType::MultipleSources:
         return "В цепи не может быть более одного источника.";
+    case ErrorType::TooManyParallelElements:
+        return "В цепи должно быть не более 10 взаимно-параллельных элементов.";
     case ErrorType::TooManyElements:
         return "Граф задан некорректно. В цепи не может быть более 100 элементов.";
     case ErrorType::TooManyEdges:
@@ -101,5 +105,4 @@ void Error::clear() {
     type = ErrorType::NoError;
     componentName.clear();
     line = 0;
-    invalidValue.clear();
 }
